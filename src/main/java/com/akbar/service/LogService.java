de38c6885ae4641c165a75c742992d66c0408010
akbar <akbar4857@qq.com>
2025-02-09 13:10:26 +0800

    日志记录接口开发完毕

---
+++ src/main/java/com/akbar/service/LogService.java
@@ -0,0 +1,12 @@
+package com.akbar.service;
+
+import com.akbar.domain.vo.PageBean;
+
+public interface LogService {
+    PageBean getLogs(int pageNum, int pageSize);
+
+    boolean cleanLogs(int days);
+
+    // 每天凌晨3点自动清理超过30天的日志
+    void autoCleanLogs();
+}
